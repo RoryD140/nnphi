a92442a47eb38e2df2aa4d72c237f01555c5d5f2
RoryD140 <roryjdouglas@gmail.com>
2017-09-14 13:16:11 -0700

    nnphi-98 | initial theming for training page, header, and footer

--- web/themes/custom/nnphi/js/src/header.js
+++ web/themes/custom/nnphi/js/src/header.js
@@ -1,9 +1,4 @@
 /**
- * @file
- * Custom scripts for theme.
- */
-
-/**
  *
  * @type {{attach: Drupal.behaviors.bcHeader.attach}}
  */
@@ -22,9 +17,3 @@ Drupal.behaviors.nnphiHeader = {
     }
   }
 };
-
-// Custom scripts go here
-// In general, it's best to use this scripts file sparingly.
-// See https://bluecoda.atlassian.net/wiki/display/BPI/Drupal+8+Best+Practices#Drupal8BestPractices-CustomTheme
-// for details
-
