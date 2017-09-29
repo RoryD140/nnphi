def415ac8074b8eb489455372ac4c9622945abf0
David Blum <davidb@bluecoda.com>
2017-09-28 19:25:39 -0700

    [NNPHI-117] Add a cache data to search page controller

--- web/modules/custom/nnphi_training/src/Controller/InstantSearch.php
+++ web/modules/custom/nnphi_training/src/Controller/InstantSearch.php
@@ -2,12 +2,16 @@
 
 namespace Drupal\nnphi_training\Controller;
 
+use Drupal\Core\Cache\Cache;
 use Drupal\Core\Controller\ControllerBase;
 
 class InstantSearch extends ControllerBase {
   public function page() {
     return [
       '#theme' => 'nnphi_training_search_page',
+      '#cache' => [
+        'max-age' => Cache::PERMANENT,
+      ],
     ];
   }
 }
