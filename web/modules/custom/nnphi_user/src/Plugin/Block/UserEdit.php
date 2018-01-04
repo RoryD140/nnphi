731a9b9846ec2ffbf05133ca14df4758b78094b4
David Blum <davidb@bluecoda.com>
2018-01-04 14:40:53 -0800

    [NNPHI-178] Use panels for user edit page, add cancel button to user profile edit form

--- web/modules/custom/nnphi_user/src/Plugin/Block/UserEdit.php
+++ web/modules/custom/nnphi_user/src/Plugin/Block/UserEdit.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\nnphi_user\Plugin\Block;
 
+use Drupal\Core\Annotation\Translation;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Entity\EntityFormBuilderInterface;
 use Drupal\Core\Session\AccountInterface;
@@ -13,8 +15,12 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * Provides a Profile Block.
  *
  * @Block(
- *  id = "user_edit",
- *  admin_label = @Translation("User Profile Edit Block"),
+ *  id="user_edit",
+ *  category=@Translation("User"),
+ *  admin_label=@Translation("User edit form"),
+ *  context = {
+ *   "user" = @ContextDefinition("entity:user", label = @Translation("User"))
+ *  }
  * )
  */
 class UserEdit extends BlockBase implements ContainerFactoryPluginInterface {
@@ -25,19 +31,13 @@ class UserEdit extends BlockBase implements ContainerFactoryPluginInterface {
   protected $entityFormBuilder;
 
   /**
-   * @var \Drupal\Core\Session\AccountInterface
-   */
-  protected $currentUser;
-
-  /**
    * @var \Drupal\Core\Entity\EntityStorageInterface
    */
   protected $userStorage;
 
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityFormBuilderInterface $entity_form_builder, AccountInterface $current_user, EntityStorageInterface $user_storage) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityFormBuilderInterface $entity_form_builder, EntityStorageInterface $user_storage) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->entityFormBuilder = $entity_form_builder;
-    $this->currentUser = $current_user;
     $this->userStorage = $user_storage;
   }
 
@@ -47,14 +47,13 @@ class UserEdit extends BlockBase implements ContainerFactoryPluginInterface {
       $plugin_id,
       $plugin_definition,
       $container->get('entity.form_builder'),
-      $container->get('current_user'),
       $container->get('entity_type.manager')->getStorage('user')
     );
   }
 
   public function build() {
-    $user = $this->userStorage->load($this->currentUser->id());
-    return $this->entityFormBuilder->getForm($user);
+    $account = $this->getContextValue('user');
+    $build['form'] = $this->entityFormBuilder->getForm($account);
     CacheableMetadata::createFromObject($account)
       ->applyTo($build);
     $build['#cache']['keys'] = ['user', 'user_edit', $account->id()];
