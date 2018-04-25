e24bad36eeeed2db66482aedc62cf35f28b741d6
David Blum <davidblumdeveloper@gmail.com>
2018-04-24 18:59:33 -0700

    Make popular trainings block actually show popular trainings

--- web/modules/custom/nnphi_training/src/Plugin/Block/NewPopular.php
+++ web/modules/custom/nnphi_training/src/Plugin/Block/NewPopular.php
@@ -5,6 +5,7 @@ namespace Drupal\nnphi_training\Plugin\Block;
 use Drupal\Core\Annotation\Translation;
 use Drupal\Core\Block\Annotation\Block;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Database\Connection;
 use Drupal\Core\Entity\EntityViewBuilderInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\node\NodeInterface;
@@ -29,13 +30,22 @@ class NewPopular extends BlockBase implements ContainerFactoryPluginInterface {
    */
   protected $nodeStorage;
 
+  /**
+   * @var \Drupal\Core\Entity\EntityViewBuilderInterface
+   */
   protected $nodeViewer;
 
+  /**
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
   public function __construct(array $configuration, $plugin_id, $plugin_definition, NodeStorageInterface $nodeStorage,
-                              EntityViewBuilderInterface $entityViewBuilder) {
+                              EntityViewBuilderInterface $entityViewBuilder, Connection $database) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->nodeStorage = $nodeStorage;
     $this->nodeViewer = $entityViewBuilder;
+    $this->database = $database;
   }
 
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
@@ -44,7 +54,8 @@ class NewPopular extends BlockBase implements ContainerFactoryPluginInterface {
       $plugin_id,
       $plugin_definition,
       $container->get('entity_type.manager')->getStorage('node'),
-      $container->get('entity_type.manager')->getViewBuilder('node')
+      $container->get('entity_type.manager')->getViewBuilder('node'),
+      $container->get('database')
     );
   }
 
@@ -97,7 +108,7 @@ class NewPopular extends BlockBase implements ContainerFactoryPluginInterface {
     $new_nids = $query
       ->condition('type', 'training')
       ->condition('status', NodeInterface::PUBLISHED)
-      ->range(0, 3)
+      ->range(0, 10)
       ->sort('created', 'DESC')
       ->execute();
     if (empty($new_nids)) {
@@ -115,7 +126,30 @@ class NewPopular extends BlockBase implements ContainerFactoryPluginInterface {
    * @return array
    */
   protected function getPopularTrainings() {
-    // Placeholder.
-    return $this->getNewTrainings();
+    // Popular trainings according to flag count.
+    $nids = $this->database->queryRange("SELECT fc.entity_id FROM {flag_counts} fc
+                                              INNER JOIN {node_field_data} n ON n.nid = fc.entity_id
+                                              WHERE entity_type = 'node'
+                                              AND flag_id = 'bookmark'
+                                              AND n.type = 'training'
+                                              AND n.status = 1
+                                              ORDER BY fc.count DESC", 0, 10)->fetchCol();
+    if (empty($nids)) {
+      return [
+        '#cache' => [
+          'tags' => ['node_list'],
+        ],
+      ];
+    }
+    $popular = $this->nodeStorage->loadMultiple($nids);
+    return $this->nodeViewer->viewMultiple($popular, 'mini');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheMaxAge() {
+    // Cache for 15 minutes max.
+    return 900;
   }
 }
