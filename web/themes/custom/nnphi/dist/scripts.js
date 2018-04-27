d5ab9d8f4f3a5719ca553fe79f2a9f79dbb81013
RoryD140 <roryjdouglas@gmail.com>
2018-04-27 10:53:28 -0700

    style-updates | Style and menu updates per feedback

--- web/themes/custom/nnphi/dist/scripts.js
+++ web/themes/custom/nnphi/dist/scripts.js
@@ -15,6 +15,11 @@
 
       $(context).find('.menu-btn').once('nnphi-header').each(function() {
         this.addEventListener('click', function() {
+          // If the user account menu is hidden (small screen sizes), prepend it to menu and show it
+          if($('#block-useraccountmenu-2:visible').length === 0) {
+            $('#block-useraccountmenu-2').prependTo($('.main-nav')).show();
+          }
+
           // Toggles both menu buttons
           $('.menu-btn').toggleClass('menu-btn-open');
           // Toggles menu
