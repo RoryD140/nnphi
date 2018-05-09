4c3b6086f2b39a6df7fee6dce856e48260766aa6
RoryD140 <roryjdouglas@gmail.com>
2018-05-09 15:23:11 -0700

    micro-quiz-bg | add option for white background on micro quiz

--- web/modules/custom/nnphi_micro_assessment/src/Plugin/Block/RandomQuiz.php
+++ web/modules/custom/nnphi_micro_assessment/src/Plugin/Block/RandomQuiz.php
@@ -8,6 +8,7 @@ use Drupal\Core\Block\Annotation\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * @Block(
@@ -36,6 +37,42 @@ class RandomQuiz extends BlockBase implements ContainerFactoryPluginInterface {
     );
   }
 
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+            'white_background' => 0,
+        ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+    $form['white_background'] = [
+        '#type' => 'checkbox',
+        '#title' => $this->t('White Background'),
+        '#description' => $this->t('Check to enable white background'),
+        '#default_value' => $this->configuration['white_background'],
+        '#weight' => '0',
+    ];
+
+    return $form;
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    $this->configuration['white_background'] = $form_state->getValue('white_background');
+    parent::submitConfigurationForm($form, $form_state);
+  }
+
+
   /**
    * {@inheritdoc}
    */
@@ -43,6 +80,7 @@ class RandomQuiz extends BlockBase implements ContainerFactoryPluginInterface {
     $id = Html::getUniqueId('random-quiz');
     $build = [];
     $build['#attached']['library'][] = 'nnphi_micro_assessment/random_quiz';
+    $build['#attributes']['class'][] = $this->configuration['white_background'] === 1 ? 'white-bg' : 'gradient-bg';
     $build['markup'] = [
       '#type' => 'inline_template',
       '#template' => "<div id='$id' class='quiz-wrapper random-quiz js-pre-ajax'></div>",
