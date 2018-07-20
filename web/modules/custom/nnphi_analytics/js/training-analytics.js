fe862cbc52ba6bae47c76d6055817753fffebf5a
RoryD140 <roryjdouglas@gmail.com>
2018-07-20 09:30:45 -0700

    Analytics dashboard fix

--- web/modules/custom/nnphi_analytics/js/training-analytics.js
+++ web/modules/custom/nnphi_analytics/js/training-analytics.js
@@ -1,21 +1,23 @@
-(function ($, Drupal, ga) {
-  'use strict';
+// Check to make sure that google analytics object exists — it doesn't if you're logged in
+if(typeof ga !== 'undefined') {
+  (function ($, Drupal, ga) {
+    'use strict';
 
-  Drupal.behaviors.trainingAnalytics = {
-    attach: function (context, settings) {
+    Drupal.behaviors.trainingAnalytics = {
+      attach: function (context, settings) {
 
-      // Track clicks on "start learning" on trainings
-      $('.training-hero-btn a').once('training-hero-click').click(function(){
-        // These links currently open in new tabs, so we don't have to preventDefault to ensure this goes through
-        ga('send', {
-          hitType: 'event',
-          eventCategory: 'Start Learning Click',
-          eventAction: 'click',
-          eventLabel: $('h1 span').html(),
+        // Track clicks on "start learning" on trainings
+        $('.training-hero-btn a').once('training-hero-click').click(function () {
+          // These links currently open in new tabs, so we don't have to preventDefault to ensure this goes through
+          ga('send', {
+            hitType: 'event',
+            eventCategory: 'Start Learning Click',
+            eventAction: 'click',
+            eventLabel: $('h1 span').html(),
+          });
         });
-      });
-    }
-  };
-})(jQuery, Drupal, ga);
-
+      }
+    };
+  })(jQuery, Drupal, ga);
+}
 
